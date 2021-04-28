4161604768ccb27baee7ad096fa21f1d69bec13a
Elvis Nhat <kenkentrang195@gmail.com>
2021-04-28 15:57:36 +0700

    1234

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+// use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
